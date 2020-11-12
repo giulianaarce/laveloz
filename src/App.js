31415e06a213de3c8ae970e9f4804ebcee769963
giulianaarce <arceg45@gmail.com>
2020-11-12 13:39:40 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+
+  
+
+  render() {
+    return (
+      <h1></h1>
+    )
+  }
+}
+
