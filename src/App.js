9b8ab0f15fe933afa8f6751016b74b85f1f08845
giulianaarce <arceg45@gmail.com>
2020-11-12 14:36:25 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -15,12 +15,9 @@ export default class App extends React.Component {
       
     }
   }
-
-  
-
   render() {
     return (
-      <h1>Proyecto Final.</h1>
+      <h1>Proyecto Final</h1>
     )
   }
 }
