e5c19f57cc6859665d1a56b6e2b126df8c48ea76
esteban.acevedo <esteban.acevedo.98@hotmail.com>
2020-11-18 20:25:33 -0300

    Commit

--- src/Components/Carrusel.js
+++ src/Components/Carrusel.js
@@ -6,31 +6,19 @@ export default class Carrusel extends React.Component {
     return (
       <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
         <ol className="carousel-indicators">
-          <li data-target="#carouselExampleControls" data-slide-to={0} className="active" />
-          <li data-target="#carouselExampleControls" data-slide-to={1} />
-          <li data-target="#carouselExampleControls" data-slide-to={2} />
-          <li data-target="#carouselExampleControls" data-slide-to={3} />
-          <li data-target="#carouselExampleControls" data-slide-to={4} />
-          <li data-target="#carouselExampleControls" data-slide-to={5} />
-          <li data-target="#carouselExampleControls" data-slide-to={6} />
-          
-        
+          {this.props.images.map((image,i)=>{
+            return(
+              <li data-target="#carouselExampleControls" data-slide-to={i} className={`${i===0? "active":""}`} />
+            )
+          })}
         </ol>
         <div className="carousel-inner">
-          {this.props.images.map((image) => {
-            if (image.imgUrl[0]) {
-              return (
-                <div className="carousel-item active">
+          {this.props?.images?.map((image,i) => {
+              return(
+              <div className={`carousel-item ${i===0? "active":""}`}>
                   <img src={image.imgUrl} className="d-block w-100" alt="imagen" />
                 </div>
               )
-            } else {
-              return (
-                <div className="carousel-item">
-                  <img src={image.imgUrl} className="d-block w-100" alt="imagen" />
-                </div>
-                )
-                }
           })}
         </div>
         <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
