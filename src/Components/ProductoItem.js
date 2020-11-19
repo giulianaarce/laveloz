46576abbead8b1b1f86269126bd3e7aeaf891943
giulianaarce <arceg45@gmail.com>
2020-11-19 11:57:21 -0300

    carrusel

--- src/Components/ProductoItem.js
+++ src/Components/ProductoItem.js
@@ -1,20 +1,5 @@
 import React from 'react'
 
-
-            {/*<div className="card" style={{ width: '25rem' }}>
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{this.props.title}</h5>
-                    <ul className="list-group list-group-flush">
-                       <li className="list-group-item">{this.props.categoria}</li>
-                        <li className="list-group-item">{this.props.descripcion}</li>
-                        <li className="list-group-item">{this.props.color}</li>
-                        <li className="list-group-item">${this.props.precio}</li>
-                    </ul>
-                </div>
-                <p className="card-text">{this.props.marketing}</p>
-            </div>*/}
-
 export default class ProductoItem extends React.Component {
     render() {
         return (
