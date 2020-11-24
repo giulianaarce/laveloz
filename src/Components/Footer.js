a463f3b64c0e9ba0a512e330bfd2b64016ff2117
giulianaarce <arceg45@gmail.com>
2020-11-24 11:22:30 -0300

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,13 +5,13 @@ export default class Footer extends React.Component{
     render(){
         return(
             <div className="footer">
-                    <a href="https://www.instagram.com/">
+                    <a href="https://www.instagram.com/" target="_blank">
                         <AiOutlineInstagram size='5rem' style={{paddingRight:'20px'}}/>
                     </a>
-                    <a href="https://www.facebook.com/La-Veloz-Carpinter%C3%ADa-476578962393306">
+                    <a href="https://www.facebook.com/La-Veloz-Carpinter%C3%ADa-476578962393306" target="_blank">
                         <FaFacebookSquare size='4rem'style={{paddingRight:'20px'}}/>
                     </a>
-                    <a href="https://twitter.com/">
+                    <a href="https://twitter.com/" target="_blank">
                         <FaTwitter size='4rem'/>
                     </a>
             </div>
