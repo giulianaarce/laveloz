098c0b6638b62adeec92d306ccfdc613b2078494
Florencia22 <f.gomez2288@gmail.com>
2020-11-25 12:34:42 -0300

    stilos a las cards

--- src/Components/Productos.js
+++ src/Components/Productos.js
@@ -17,10 +17,18 @@ export default class Productos extends React.Component {
         <div className="contenedor-home">
         {this.props.productos.map((producto) => {
           return (
-            <div className="contenedor-productos">
+            <div className="contenedor-productos" style={{display: "flex", flexwrap: 'wrap',borderradius: '35px 0px 35px 0px',
+            mozborderradius: '35px 0px 35px 0px',
+            webkitborderradius: '35px 0px 35px 0px',
+            border: '2px solid #5878ca',
+            aligncontent: 'space-between',
+            flexGrow:'5px',
+            
+
+            }}>
               <div className="card" style={{width: '18rem'}}>
                 <img src={producto.imgUrl} className="card-img-top" alt="..." />
-                <div className="card-body">
+                <div className="card-body" >
                     <h5 className="card-title">{producto.producto}</h5>
                     <p className="card-text">
                         {producto.descripcion}.{producto.marketing}<br/>
