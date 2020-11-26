bbde0a8ba47f1db11f08a88e142c781985ed32a2
giulianaarce <arceg45@gmail.com>
2020-11-25 21:01:49 -0300

    precios

--- src/Components/Productos.js
+++ src/Components/Productos.js
@@ -18,7 +18,8 @@ export default class Productos extends React.Component {
           <Categorias categorias={this.props.categorias} sendCat={this.props.categ}/>
         <div className="contenedor-home">
         {this.props.productos.map((producto) => {
-          return (
+          if(producto.descuento){
+            return (
             <div className="contenedor-productos" style={{display: "flex", flexwrap: 'wrap',borderradius: '35px 0px 35px 0px',
             mozborderradius: '35px 0px 35px 0px',
             webkitborderradius: '35px 0px 35px 0px',
@@ -32,17 +33,49 @@ export default class Productos extends React.Component {
                 <img src={producto.imgUrl} className="card-img-top" alt="..." />
                 <div className="card-body" >
                     <h5 className="card-title">{producto.producto}</h5>
-                    <p className="card-text">
+                    <div className="card-text">
                         {producto.descripcion}.{producto.marketing}<br/>
                         Colores:{producto.color}<br/>
                         Dimensiones:{producto.dimension}<br/>
-                        <strong>${producto.precio}</strong>
-                    </p>
-                    <Link to="/producto-detalle"><button value={producto._id} onClick={this.handler_id}>Detalle</button></Link>
+                        Descuento: {producto.descuento}% <br/>
+                        Precio Antes<p className="text-danger" ><s>${producto.precio - (producto.precio / 10)}</s><br/></p>
+                        Precio Ahora<p className="text-success" ><strong>${producto.precio}</strong></p>
+                    </div>
+                    <div>
+                      <Link to="/producto-detalle"><button value={producto._id} onClick={this.handler_id}>Detalle</button></Link>
+                    </div>
                 </div>
             </div>
             </div>
-          )
+          )}else{
+            return (
+              <div className="contenedor-productos" style={{display: "flex", flexwrap: 'wrap',borderradius: '35px 0px 35px 0px',
+              mozborderradius: '35px 0px 35px 0px',
+              webkitborderradius: '35px 0px 35px 0px',
+              border: '2px solid #5878ca',
+              justifycontent: 'space-around',
+              flexGrow:'5px',
+              
+  
+              }}>
+                <div className="card" style={{width: '18rem'}}>
+                  <img src={producto.imgUrl} className="card-img-top" alt="..." />
+                  <div className="card-body" >
+                      <h5 className="card-title">{producto.producto}</h5>
+                      <p className="card-text">
+                          {producto.descripcion}.{producto.marketing}<br/>
+                          Colores:{producto.color}<br/>
+                          Dimensiones:{producto.dimension}<br/><br/>
+                          Precio: <strong>${producto.precio}</strong>
+                      </p>
+                      <div>
+                        <Link to="/producto-detalle"><button value={producto._id} onClick={this.handler_id}>Detalle</button></Link>
+                      </div>
+                  </div>
+              </div>
+              </div>
+            )
+          }
         })}
         </div>
       </>
