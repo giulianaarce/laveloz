73aa209fc35ccaf07f7fa23d078b270833610f58
giulianaarce <arceg45@gmail.com>
2020-11-26 22:22:45 -0300

    cambio-wa

--- src/Components/Whatsapp.js
+++ src/Components/Whatsapp.js
@@ -4,7 +4,7 @@ export default class ProductoFav extends React.Component {
     render() {
         return (
             <div className="whatsapp1" >
-                <a href="https://wa.me/543512110060?text=Me%20gustaría%20saber%20el%20precio%20de%20un%20mueble" className="whatsapp" target="_blank"> 
+                <a href="https://wa.me/54---------?text=Me%20gustaría%20saber%20el%20precio%20de%20un%20mueble" className="whatsapp" target="_blank"> 
                     <i className="fa fa-whatsapp whatsapp-icon"></i>
                 </a>
             </div>
